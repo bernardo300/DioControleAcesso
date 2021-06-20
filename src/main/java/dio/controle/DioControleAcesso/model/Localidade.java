d43c14120096ab97b8743ce0938c5df84070fd3c
bernardo300 <bernardolopes.luz@hotmail.com>
2021-06-20 20:20:04 -0300

    classe Localidade e Usuario

---
+++ src/main/java/dio/controle/DioControleAcesso/model/Localidade.java
@@ -0,0 +1,18 @@
+package dio.controle.DioControleAcesso.model;
+
+import lombok.*;
+
+import javax.persistence.OneToMany;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Localidade {
+    private long id;
+    @OneToMany
+    private NivelAcesso nivelAcesso;
+    private String descricao;
+}
